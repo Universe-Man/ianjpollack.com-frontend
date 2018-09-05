2ec10342b7865429c000340f1c45662db68b89fe
github username <ianjpollack@gmail.com>
2018-09-05 19:16:29 -0400

    home page done

--- src/containers/DisplayContainer.js
+++ src/containers/DisplayContainer.js
@@ -1,27 +1,27 @@
 import React from 'react';
+import '../assets/DisplayContainer.css';
+import homeImage from '../images/ian_tie_center_whitebg.jpg';
 
 class DisplayContainer extends React.Component {
-  constructor(){
-    super();
-    this.state = {}
-  }
 
   render() {
     console.log('state in the display', this.props.state);
     return (
       <React.Fragment>
         {(this.props.state.renderHome === true) ? (
-          <div>
-            renderHome
+          <div id="homePage">
+            <h2 id="homeText1">Ian Pollack</h2>
+            <h2 id="homeText2">thinks you're cool</h2>
+            <img src={homeImage} alt="Ian tie center" width="1200" height="800" ></img>
           </div>
         ) : (null)}
         {(this.props.state.renderAbout === true) ? (
-          <div>
+          <div id="aboutPage">
             renderAbout
           </div>
         ) : (null)}
         {(this.props.state.renderContact === true) ? (
-          <div>
+          <div id="contactPage">
             renderContact
           </div>
         ) : (null)}
